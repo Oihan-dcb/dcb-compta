b0e6d0a8e4dd9d116548b2277020a1d59f496425
Oihan Campandegui <oihancampandegui@MacBook-Pro-2.local>
2026-03-15 03:58:55 +0100

    chore: cache bust pour forcer nouveau bundle Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-// v000636
+// v025819
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
