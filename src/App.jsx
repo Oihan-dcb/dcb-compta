fd93dbeb7c6609dc11c8a6b092a70d8129c0f04e
Oihan-dcb <oihan@destinationcotebasque.com>
2026-03-21 12:41:01 +0100

    refactor: supprimer onglet Factures Auto

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import PageReservations from './pages/PageReservations'
 import PageBanque from './pages/PageBanque'
 import PageMatching from './pages/PageMatching'
 import PageRapprochement from './pages/PageRapprochement'
-import PageFacturesAE from './pages/PageFacturesAE'
 import PageFactures from './pages/PageFactures'
 import PageConfig from './pages/PageConfig'
 import PageAutoEntrepreneurs from './pages/PageAutoEntrepreneurs'
@@ -41,7 +40,7 @@ export default function App() {
             <Route path="/banque" element={<PageBanque />} />
             <Route path="/matching" element={<PageMatching />} />
               <Route path="/rapprochement" element={<PageRapprochement />} />
-            <Route path="/factures-ae" element={<PageFacturesAE />} />
+} />
             <Route path="/factures" element={<PageFactures />} />
             <Route path="/import" element={<PageImport />} />
             <Route path="/portail-ae/:token" element={<PortailAEWrapper />} />
