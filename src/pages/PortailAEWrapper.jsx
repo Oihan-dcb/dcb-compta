44316875b4eeded4ee3209545e9260da57aa1d2b
Oihan-dcb <oihan@destinationcotebasque.com>
2026-03-21 19:10:56 +0100

    refactor: supprimer PortailAEWrapper obsolète

--- src/pages/PortailAEWrapper.jsx
+++
@@ -1,7 +0,0 @@
-import { useParams } from 'react-router-dom'
-import PortailAE from './PortailAE'
-
-export default function PortailAEWrapper() {
-  const { token } = useParams()
-  return <PortailAE token={token} />
-}
